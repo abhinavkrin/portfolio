03a57682747c7af4add3995f7039e4822453023f
Abhinav Kumar <15830206+abhinavkrin@users.noreply.github.com>
2021-06-13 02:16:27 +0530

    Update blogposts.js

--- src/data/blogposts.js
+++ src/data/blogposts.js
@@ -1,11 +1,11 @@
 export const blogposts = [{
     title: "Stress Testing. Finding edge cases where our algorithm fails",
     excerpt: "We will see a powerful technique, called Stress Testing, to test the correctness of our programming solution. Often, in competitive programming, We come across scenarios where our solution gives us wrong answers, although the solution seems to be 100% correct. Sometimes, we are stuck in some edge cases. Well, a popular competitive programming platform like codechef does not reveal those test cases. How to find out that? Even after scratching your head for hours, you fail to find it out. You can try “Stress Testing“....",
-    link: "https://avitechlab.com/how-to-find-out-edge-cases-where-your-algorithm-fails-using-stress-testing/",
-    featureImage: "https://i1.wp.com/avitechlab.com/wp-content/uploads/2020/06/Stress-Testing.jpg?resize=512%2C512&ssl=1"
+    link: "https://avitechlab.com/blogs/stress-testing-finding-edge-cases-where-our-algorithm-fails",
+    featureImage: "https://avitechlab.com/_next/image?url=%2Fimages%2Fuploads%2Fstress_testing_banner.jpg&w=828&q=75"
 },{
     title: "How to scrap notices from your college website using nodejs.",
     excerpt: "ver wanted to build a notice app or creating a notification system for new notices for your college? But how to get real-time notices data? Not all colleges (not mine) provides an HTTP endpoint to get college notices. However, not to worry, we can use web scrapping to extract notices directly from the college website. What is web scraping? In simple words, web scraping means to extract data from websites.…",
-    link: "https://avitechlab.com/how-to-get-notices-from-your-college-website-using-nodejs/",
-    featureImage: "https://i0.wp.com/avitechlab.com/wp-content/uploads/2020/11/Scraping-Notices-from-College-Website.png?w=540&ssl=1"
+    link: "https://avitechlab.com/blogs/how-to-get-notices-from-your-college-website-using-nodejs",
+    featureImage: "https://avitechlab.com/_next/image?url=%2Fimages%2Fuploads%2Fnotice-scrapping.jpg&w=828&q=75"
 }]
